116f4431ee188d518effeb6fe9a6fa4267ab36b9
Rahul Gupta <gupta8914@gmail.com>
2020-06-04 19:32:02 +0530

    active class fixed

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ var toggle = function (e) {
     .querySelector("#navbarSupportedContent")
     .classList.toggle("collapse");
 };
+
+(function () {
+  /* */
+  var btns = document.getElementsByClassName("nav-link");
+  for (let index = 0; index < btns.length; index++) {
+    btns[index].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active")[0];
+      current.className = current.className.replace("active", "");
+      this.className += " active";
+    });
+  }
+})();
