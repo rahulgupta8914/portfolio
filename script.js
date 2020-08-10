a116d36bf8284a43588c1c4d57da776062ec0221
Rahul Gupta <gupta8914@gmail.com>
2020-08-10 19:12:27 +0530

    active nav update on scroll

--- script.js
+++ script.js
@@ -12,7 +12,32 @@ var toggle = function () {
       var current = document.getElementsByClassName("active")[0];
       current.className = current.className.replace("active", "");
       this.className += " active";
-      toggle()
+      toggle();
     });
   }
 })();
+
+// navbar active class update update on scoll
+const sections = document.querySelectorAll("section");
+let observer = new IntersectionObserver(
+  function (entries, self) {
+    entries.map((entry) => {
+      if (entry.isIntersecting 
+        && entry.intersectionRatio > 0.6
+        ) {
+        const curActiveNav = document.querySelector(".active");
+        const updateNav = document.querySelector(
+          `a[href='#${entry.target.id}']`
+        );
+        if (curActiveNav) curActiveNav.classList.remove("active");
+        if (updateNav) updateNav.classList.add("active");
+      }
+    });
+  },
+  {
+    rootMargin: "-70px 0px 0px",
+    threshold: .6
+  }
+);
+
+sections.forEach((section) => observer.observe(section));
